da884d211f022b62373c25ffcf8c8b9ce7c03354
Nico Lindemann <nico.lindemann@proux.net>
2019-08-13 23:40:41 +0000

    style: lint

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports = {
     var coffee = require('coffee-script')
     if (coffee.helpers.isCoffee(path)) {
       return coffee.compile(src, {
-        'bare': true,
-        'inlineMap': true
+        bare: true,
+        inlineMap: true
       })
     }
     return null
